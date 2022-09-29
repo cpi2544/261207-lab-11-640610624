d6dda2830a4d822f993e91ba324bdf91d000f5ec
Your NameCpi2544 <pokpong1789@gmail.com>
2022-09-29 21:16:20 +0700

    completed

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -12,10 +12,33 @@ export default function login(req, res) {
 
     //find users with username, password
 
+    const foundUser = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
+
+    if (!foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid Username or Password" });
+
     const secret = process.env.JWT_SECRET;
 
     //sign token
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+        isAdmin: foundUser.isAdmin,
+      },
+      secret,
+      { expiresIn: "1800s" }
+    );
 
     //return response
+    return res.json({
+      ok: true,
+      username: foundUser.username,
+      isAdmin: foundUser.isAdmin,
+      token,
+    });
   }
 }
